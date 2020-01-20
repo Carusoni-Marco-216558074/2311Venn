c033473b74cb7a63d2654a26923f6c407af8cd4e
Carusoni-Marco-216558074 <marco.carusoni@gmail.com>
2020-01-20 13:26:17 -0500

    update

--- 2311Venn/src/main/java/Venn/testClass.java
+++ 2311Venn/src/main/java/Venn/testClass.java
@@ -1,44 +1,83 @@
 package Venn;
 
-import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.scene.Scene;
-import javafx.scene.SubScene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
-import javafx.stage.Stage;
- 
-public class testClass extends Application {
+import com.sun.org.apache.xalan.internal.xsltc.compiler.util.NumberType;
+import com.sun.xml.internal.bind.v2.schemagen.xmlschema.Union;
+
+//Java program to implement Slider Class 
+//using ChangeListener 
+import javafx.application.Application; 
+import javafx.beans.value.ChangeListener; 
+import javafx.beans.value.ObservableValue; 
+import javafx.geometry.Insets; 
+import javafx.scene.Scene; 
+import javafx.scene.control.Label; 
+import javafx.scene.control.Slider; 
+import javafx.scene.layout.VBox; 
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Sphere;
+import javafx.scene.text.Font;
+import javafx.stage.Stage; 
+
+public class testClass extends Application { 
+
+	public void start(Stage stage) 
+	{ 
+
+		Label l = new Label(" "); 
+
+		l.setTextFill(Color.BLACK); 
+		l.setFont(new Font("Arial", 30));
+		
+		
+		Slider slider = new Slider(); 
+		
+		Circle Ven1 = new Circle(100);
+		Ven1.setStroke(Color.DARKSLATEBLUE);
+		Ven1.setFill(Color.TRANSPARENT);
 	
-    public static void main(String[] args) {
-        launch(args);
-    }
-    
-    @Override
-    public void start(Stage primaryStage) {
-    	primaryStage.centerOnScreen();
-    	primaryStage.setResizable(false);
-        primaryStage.setTitle("Test Main Frame");
-        
-        Button btn = new Button();
-        
-        
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
- 
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
-        
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        primaryStage.setScene(new Scene(root, 1000, 720));
-        SubScene secScene = new SubScene(root, 200, 200);
-        
-        
-        primaryStage.show();
-    }
-}
+		Circle Ven2 = new Circle(100);
+		Ven2.setStroke(Color.DARKRED);
+		Ven2.setFill(Color.TRANSPARENT);
+		
+		slider.setMin(0); 
+		slider.setMax(100); 
+		slider.setValue(50); 
+
+		slider.setShowTickLabels(true); 
+		slider.setShowTickMarks(true); 
+		slider.setSnapToTicks(true);
+		slider.setBlockIncrement(10); 
+
+		slider.valueProperty().addListener( 
+			new ChangeListener<Number>() { 
+
+			public void changed(ObservableValue <? extends Number > 
+					observable, Number oldValue, Number newValue) 
+			{ 
+				Ven1.setRadius(2 * newValue.doubleValue());
+				Ven2.setRadius(2 * newValue.doubleValue());
+
+				l.setText("value: " +  newValue.intValue()); 
+			} 
+		}); 
+
+		VBox root = new VBox(); 
+
+		root.setPadding(new Insets(20)); 
+		root.setSpacing(10); 
+		root.getChildren().addAll( slider, l, Ven1, Ven2); 
+
+		stage.setTitle("Slider"); 
+
+		Scene scene = new Scene(root, 1920, 1080); 
+		stage.setScene(scene); 
+		stage.show(); 
+	} 
+
+	public static void main(String[] args) 
+	{ 
+
+		Application.launch(args); 
+	} 
+} 
