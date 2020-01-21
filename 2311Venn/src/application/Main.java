4ee87b6509f768b35569320b76b5f756d8df0951
Carusoni-Marco-216558074 <marco.carusoni@gmail.com>
2020-01-21 13:11:51 -0500

    Made JavaFX Project

---
+++ 2311Venn/src/application/Main.java
@@ -0,0 +1,90 @@
+package application;
+	
+import com.sun.javafx.geom.Shape;
+
+//Java program to implement Slider Class 
+//using ChangeListener 
+import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Sphere;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
+
+@SuppressWarnings({ "unused", "restriction" })
+public class Main extends Application {
+
+	@SuppressWarnings("static-access")
+	public void start(Stage stage) {
+
+		Slider slider = new Slider();
+
+		Circle Ven1 = new Circle(200);
+		Ven1.setStroke(Color.DARKSLATEBLUE);
+		Ven1.setFill(Color.TRANSPARENT);
+
+		Circle Ven2 = new Circle(200);
+		Ven2.setStroke(Color.DARKRED);
+		Ven2.setFill(Color.TRANSPARENT);
+
+		slider.setMin(0);
+		slider.setMax(100);
+		slider.setValue(50);
+
+		slider.setShowTickLabels(true);
+		slider.setShowTickMarks(true);
+		slider.setSnapToTicks(true);
+		slider.setBlockIncrement(10);
+
+		slider.valueProperty().addListener(new ChangeListener<Number>() {
+
+			public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+				Ven1.setRadius(4 * newValue.doubleValue());
+				Ven2.setRadius(4 * newValue.doubleValue());
+			}
+		});
+
+		BorderPane root = new BorderPane();
+
+		root.setBottom(slider);
+
+		HBox levelUp = new HBox(10);
+
+		levelUp.setAlignment(Pos.CENTER);
+
+		Insets in = new Insets(-75);
+
+		levelUp.setHgrow(Ven1, Priority.ALWAYS);
+		levelUp.setHgrow(Ven2, Priority.ALWAYS);
+
+		levelUp.setMargin(Ven1, in);
+		levelUp.setMargin(Ven2, in);
+
+		levelUp.getChildren().addAll(Ven1, Ven2);
+
+		root.setCenter(levelUp);
+
+		stage.setTitle("Slider");
+
+		Scene scene = new Scene(root, 1900, 1000);
+
+		stage.setScene(scene);
+		stage.show();
+	}
+
+	public static void main(String[] args) {
+
+		Application.launch(args);
+	}
+}
