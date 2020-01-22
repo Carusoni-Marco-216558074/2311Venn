ac65b6f92719cd090bc0f621147ecd11006d5163
Carusoni-Marco-216558074 <marco.carusoni@gmail.com>
2020-01-21 20:20:40 -0500

    Made Title

--- 2311Venn/src/application/Main.java
+++ 2311Venn/src/application/Main.java
@@ -1,5 +1,5 @@
 package application;
-	
+
 import com.sun.javafx.geom.Shape;
 
 //Java program to implement Slider Class 
@@ -12,6 +12,8 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
@@ -20,6 +22,7 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Sphere;
 import javafx.scene.text.Font;
+import javafx.scene.text.TextBuilder;
 import javafx.stage.Stage;
 
 @SuppressWarnings({ "unused", "restriction" })
@@ -27,6 +30,8 @@ public class Main extends Application {
 
 	@SuppressWarnings("static-access")
 	public void start(Stage stage) {
+
+		BorderPane root = new BorderPane();
 
 		Slider slider = new Slider();
 
@@ -39,35 +44,54 @@ public class Main extends Application {
 		Ven2.setFill(Color.TRANSPARENT);
 
 		slider.setMin(50);
-		slider.setMax(115);
-		slider.setValue(81.3);
+		slider.setMax(100);
+		slider.setValue(75);
 
 		slider.setShowTickMarks(true);
 		slider.setBlockIncrement(5);
 
+		TextField Title = new TextField("Insert Title Here");
+
+		Font font = new Font("TimesRoman", 36);
+		Title.setFont(font);
+
+		Title.setAlignment(Pos.CENTER);
+		Title.setBackground(null);
+
+		Title.setOnKeyTyped(event -> {
+			int maxCharacters = 60;
+			if (Title.getText().length() > maxCharacters)
+				event.consume();
+		});
+		
+
+		VBox TitleBox = new VBox(1);
+
+		TitleBox.getChildren().addAll(Title);
+
+		TitleBox.setAlignment(Pos.BASELINE_CENTER);
+
+		root.setTop(TitleBox);
+
 		Insets in = new Insets(-2.1 * 81.3);
-		
+
 		HBox levelUp = new HBox(10);
-		
+
 		slider.valueProperty().addListener(new ChangeListener<Number>() {
 
 			public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
 				Ven1.setRadius(4 * newValue.doubleValue());
 				Ven2.setRadius(4 * newValue.doubleValue());
-				
-				Insets in = new Insets(-2.1 * newValue.doubleValue());
 
-				
+				Insets in = new Insets(-2 * newValue.doubleValue());
+
 				levelUp.setMargin(Ven1, in);
 				levelUp.setMargin(Ven2, in);
-				
+
 			}
 		});
 
-		BorderPane root = new BorderPane();
-
 		root.setBottom(slider);
-
 
 		levelUp.setAlignment(Pos.CENTER);
 
