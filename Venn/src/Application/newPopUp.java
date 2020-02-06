af79ec3932556968a134b2d100801abc51339565
walker-jeffrey-215222003 <46364288+walker-jeffrey-215222003@users.noreply.github.com>
2020-02-06 14:03:44 -0500

    pushes after failure

--- Venn/src/Application/newPopUp.java
+++ Venn/src/Application/newPopUp.java
@@ -1,5 +1,29 @@
 package Application;
-
+import javafx.stage.*;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.VBox;
 public class newPopUp {
 
+	public static void display(String title, String message) {
+		
+		Stage window = new Stage();
+		window.setTitle(title);
+		window.setMinWidth(350);
+		window.setMinHeight(200);
+		
+		
+		Label label = new Label();
+		label.setText(message);
+		
+		VBox layout = new VBox(35);
+		layout.getChildren().addAll(label);
+		layout.setAlignment(Pos.CENTER);
+		Scene scene = new Scene(layout);
+		window.setScene(scene);
+		window.showAndWait();
+		
+		
+	}
 }
