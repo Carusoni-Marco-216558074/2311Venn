3723fd9a9e227c0d583febd2b600c3b854ebf9c9
Carusoni-Marco-216558074 <marco.carusoni@gmail.com>
2020-02-07 17:59:02 -0500

    updated titlebox form

--- Venn/src/Application/Main.java
+++ Venn/src/Application/Main.java
@@ -38,65 +38,64 @@ import javafx.scene.control.MenuItem;
 import Application.objectMaker;
 
 public class Main extends Application {
-	//test
+	// test
 	Rectangle2D screenBounds = Screen.getPrimary().getBounds();
-	//public double scalar = 1 / ((1920 * 1080) / (screenBounds.getMaxX() * screenBounds.getMaxY()));
+	// public double scalar = 1 / ((1920 * 1080) / (screenBounds.getMaxX() *
+	// screenBounds.getMaxY()));
 	double scalar = 1;
+
 	@SuppressWarnings("static-access")
 	public void start(Stage stage) {
 
 		StackPane root = new StackPane();
-		
-		ChoiceBox<String> File = new ChoiceBox<>();
-		File.getItems().addAll("File","New","Open File","Save","Print","Exit");
 
+		ChoiceBox<String> File = new ChoiceBox<>();
+		File.getItems().addAll("File", "New", "Open File", "Save", "Print", "Exit");
 
 		Menu fileMenu = new Menu("_File");
-		
+
 		MenuItem newFile = new MenuItem("New...");
-		newFile.setOnAction(e->{
+		newFile.setOnAction(e -> {
 			newPopUp.display("New", "making a new Venn Diagram...");
 		});
-		
+
 		MenuItem openFile = new MenuItem("Open...");
-		openFile.setOnAction(e->{
+		openFile.setOnAction(e -> {
 			newPopUp.display("Open", "opening a file...");
 		});
-		
+
 		MenuItem saveFile = new MenuItem("Save As...");
-		saveFile.setOnAction(e->{
+		saveFile.setOnAction(e -> {
 			newPopUp.display("Save As", "saving a file...");
 		});
-		
+
 		MenuItem importFile = new MenuItem("Import...");
-		importFile.setOnAction(e->{
+		importFile.setOnAction(e -> {
 			newPopUp.display("Import", "importing a file...");
 		});
-		
+
 		MenuItem exportFile = new MenuItem("Export...");
-		exportFile.setOnAction(e->{
+		exportFile.setOnAction(e -> {
 			newPopUp.display("Export", "exporting a file...");
 		});
-		
+
 		MenuItem searchFile = new MenuItem("Search");
-		searchFile.setOnAction(e->{
+		searchFile.setOnAction(e -> {
 			newPopUp.displaySearch();
 		});
-		
+
 		MenuItem exitFile = new MenuItem("Exit");
-		exitFile.setOnAction(e->{
+		exitFile.setOnAction(e -> {
 			confirmBox.display("Venn Diagram Maker", "are you sure you want to close?");
 			e.consume();
-			
-			if(confirmBox.answer==true) {
+
+			if (confirmBox.answer == true) {
 				stage.close();
 			}
 		});
-		
+
 		// search button
-		
-		
-		
+
 		fileMenu.getItems().add(newFile);
 		fileMenu.getItems().add(openFile);
 		fileMenu.getItems().add(new SeparatorMenuItem());
@@ -106,17 +105,12 @@ public class Main extends Application {
 		fileMenu.getItems().add(importFile);
 		fileMenu.getItems().add(exportFile);
 		fileMenu.getItems().add(new SeparatorMenuItem());
-	
+
 		fileMenu.getItems().add(new MenuItem("Print"));
 		fileMenu.getItems().add(new SeparatorMenuItem());
 		fileMenu.getItems().add(exitFile);
-		
-		
-		
-		
-		
-		
-		//edit menu
+
+		// edit menu
 		Menu editMenu = new Menu("_Edit");
 		editMenu.getItems().add(new MenuItem("Undo"));
 		editMenu.getItems().add(new MenuItem("Redo"));
@@ -126,35 +120,20 @@ public class Main extends Application {
 		editMenu.getItems().add(new MenuItem("Paste"));
 		editMenu.getItems().add(new SeparatorMenuItem());
 		editMenu.getItems().add(new MenuItem("Delete"));
-		
-		//view menu
+
+		// view menu
 		Menu viewMenu = new Menu("_View");
 		viewMenu.getItems().add(new MenuItem("insert"));
-		
-		//help menu
+
+		// help menu
 		Menu helpMenu = new Menu("_Help");
 		helpMenu.getItems().add(searchFile);
 		helpMenu.getItems().add(new MenuItem("About"));
 		helpMenu.getItems().add(new MenuItem("Tutorial"));
-		
-		
-		//mainMenuBar
+
+		// mainMenuBar
 		MenuBar menuBar = new MenuBar();
-		menuBar.getMenus().addAll(fileMenu,editMenu,viewMenu,helpMenu);
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+		menuBar.getMenus().addAll(fileMenu, editMenu, viewMenu, helpMenu);
 
 		ColorPicker cpV1 = new ColorPicker();
 		ColorPicker cpV2 = new ColorPicker();
@@ -162,24 +141,24 @@ public class Main extends Application {
 		cpV2.setValue(Color.rgb(150, 0, 0, 0.2));
 
 		Slider sizeSlider = new Slider();
-		objectMaker.createSlider(sizeSlider, scalar-0.17);
+		objectMaker.createSlider(sizeSlider, scalar - 0.17);
 
 		Slider Ven1Slider = new Slider();
-		objectMaker.createSlider(Ven1Slider, scalar-0.17);
+		objectMaker.createSlider(Ven1Slider, scalar - 0.17);
 
 		Slider Ven2Slider = new Slider();
-		objectMaker.createSlider(Ven2Slider, scalar-0.17);
+		objectMaker.createSlider(Ven2Slider, scalar - 0.17);
 
 		Circle Ven1 = new Circle(4 * sizeSlider.getValue());// change to screen size
 		Ven1.setStroke(Color.BLACK);
 		Ven1.setFill(Color.rgb(0, 0, 150, 0.2));
 
-		double radius = Ven1.getRadius()*scalar;
+		double radius = Ven1.getRadius() * scalar;
 		double mainFontSize = Ven1.getRadius() / (scalar * 13);
 		double smallFont = mainFontSize / 2;
-		//double subTitleTranslate = 0.35*scalar;
+		// double subTitleTranslate = 0.35*scalar;
 		double subTitleTranslate = 0;
-		
+
 		Font subTitleFont = new Font("Arial Bold", mainFontSize);
 		Font chkBoxFont = new Font("Arial", smallFont);
 		Font Titlefont = new Font("Arial Bold", 36);
@@ -219,11 +198,11 @@ public class Main extends Application {
 
 		// subTitle.setTranslateY(-(radius * 1.25));
 		//
-		//ven1Title.setTranslateY(-(radius * 1.25)/scalar);
-		//ven2Title.setTranslateY(-(radius * 1.25)/scalar);
+		// ven1Title.setTranslateY(-(radius * 1.25)/scalar);
+		// ven2Title.setTranslateY(-(radius * 1.25)/scalar);
 		//
-		//ven1Title.setTranslateX((-(radius * subTitleTranslate)*scalar));
-		//ven2Title.setTranslateX((radius * subTitleTranslate)*scalar);
+		// ven1Title.setTranslateX((-(radius * subTitleTranslate)*scalar));
+		// ven2Title.setTranslateX((radius * subTitleTranslate)*scalar);
 
 		VBox TitleBox = new VBox(1);
 
@@ -231,60 +210,57 @@ public class Main extends Application {
 		Title.setFont(Titlefont);
 		Title.setAlignment(Pos.CENTER);
 		Title.setBackground(null);
-		TitleBox.getChildren().addAll(Title);
+		TitleBox.getChildren().addAll(menuBar, Title);
 		TitleBox.setAlignment(Pos.BASELINE_CENTER);
-		//TitleBox.setTranslateY(-(30 * scalar));
-		//TitleBox.setTranslateX((50 * scalar));
+		// TitleBox.setTranslateY(-(30 * scalar));
+		// TitleBox.setTranslateX((50 * scalar));
 
-	//	root.setTop(TitleBox);
+		// root.setTop(TitleBox);
 
 		Insets in = new Insets(-(151 * scalar));
 
 		VBox leftPanal = new VBox(1);
-		
+
 		Separator leftSeparator = new Separator();
 		leftSeparator.setOrientation(Orientation.VERTICAL);
-		
-		leftPanal.getChildren().addAll(leftSeparator, text, Numbers, chkTitle, chkSub, cpV1, cpV2, Ven1Slider, Ven2Slider, sizeSlider);
+
+		leftPanal.getChildren().addAll(leftSeparator, text, Numbers, chkTitle, chkSub, cpV1, cpV2, Ven1Slider,
+				Ven2Slider, sizeSlider);
 		leftPanal.setAlignment(Pos.BOTTOM_LEFT);
-		
-		
-		
-		//root.setLeftAnchor(leftPanal, 8.0);
+
+		// root.setLeftAnchor(leftPanal, 8.0);
 		HBox divider = new HBox();
-		divider.getChildren().addAll(leftPanal,leftSeparator);
+		divider.getChildren().addAll(leftPanal, leftSeparator);
 		root.setAlignment(divider, Pos.BASELINE_LEFT);
-		//root.setAlignment(leftSeparator, Pos.BASELINE_LEFT);
+		// root.setAlignment(leftSeparator, Pos.BASELINE_LEFT);
 
 		HBox MainCenter = new HBox(10);
 
 		MainCenter.setMargin(Ven1, in);
 		MainCenter.setMargin(Ven2, in);
-		
+
 		VBox centerBox = new VBox();
 		centerBox.setAlignment(Pos.CENTER);
 		MainCenter.setAlignment(Pos.BOTTOM_CENTER);
-		//MainCenter.setAlignment(Pos.CENTER);
+		// MainCenter.setAlignment(Pos.CENTER);
 		MainCenter.setTranslateY(20);
 		MainCenter.getChildren().addAll(Ven1, Ven2);
 		centerBox.getChildren().addAll(MainCenter, subTitle);
-	//	root.setCenter(centerBox);
-	//	root.getChildren().add(centerBox );
-		
-		/*thing below makes the panal layer and titlebox layer transparent to the mouse
+		// root.setCenter(centerBox);
+		// root.getChildren().add(centerBox );
+
+		/*
+		 * thing below makes the panal layer and titlebox layer transparent to the mouse
 		 * so that mouse can be used on every layer and not blocking each other
 		 * otherwise mouse wont work on the ui elements
 		 */
 		leftPanal.setPickOnBounds(false);
 		TitleBox.setPickOnBounds(false);
-	
-		root.getChildren().addAll(  centerBox,  TitleBox, leftPanal );
-	
-		
-		
-		
+
+		root.getChildren().addAll(centerBox, TitleBox, leftPanal);
+
 		// listeners - figure out how to better format, maybe separate file?
-		
+
 		sizeSlider.valueProperty().addListener(new ChangeListener<Number>() {
 
 			public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
@@ -301,8 +277,8 @@ public class Main extends Application {
 				ven1Title.setTranslateY(-(radius * 1.25));
 				ven2Title.setTranslateY(-(radius * 1.25));
 				//
-				ven1Title.setTranslateX((-(radius * subTitleTranslate)*scalar));
-				ven2Title.setTranslateX((radius * subTitleTranslate)*scalar);
+				ven1Title.setTranslateX((-(radius * subTitleTranslate) * scalar));
+				ven2Title.setTranslateX((radius * subTitleTranslate) * scalar);
 
 				Ven1Slider.setValue(newValue.doubleValue());
 				Ven2Slider.setValue(newValue.doubleValue());
@@ -327,7 +303,7 @@ public class Main extends Application {
 				MainCenter.setMargin(Ven1, in);
 				ven1Title.setTranslateY(-(radius * 1.25));
 				//
-				ven1Title.setTranslateX((-(radius * subTitleTranslate)*scalar));
+				ven1Title.setTranslateX((-(radius * subTitleTranslate) * scalar));
 				Font changedSubFont = new Font("Arial Bold", radius / (scalar * 13));
 				ven1Title.setFont(changedSubFont);
 
@@ -347,7 +323,7 @@ public class Main extends Application {
 				MainCenter.setMargin(Ven2, in);
 				ven2Title.setTranslateY(-(radius * 1.25));
 				//
-				ven2Title.setTranslateX((radius * subTitleTranslate)*scalar);
+				ven2Title.setTranslateX((radius * subTitleTranslate) * scalar);
 				Font changedSubFont = new Font("Arial Bold", radius / (scalar * 13));
 				ven2Title.setFont(changedSubFont);
 
@@ -401,11 +377,9 @@ public class Main extends Application {
 
 					if (text.isSelected() == true) {
 						System.out.println("text");
-						
-						
+
 					} else {
 						System.out.println("Number");
-						
 
 					}
 				}
@@ -500,14 +474,14 @@ public class Main extends Application {
 
 			Ven2.setFill(Color.rgb(red, green, blue, 0.2));
 
-		});		
-		
+		});
 
 		stage.setTitle("Slider");
-		//Scene scene = new Scene(root, screenBounds.getMaxX() - (30 * scalar), screenBounds.getMaxY() - (60 * scalar));
-	//	stage.setResizable(false);
-		Scene scene = new Scene(root, screenBounds.getMaxX() -20, screenBounds.getMaxY() -30);
-		
+		// Scene scene = new Scene(root, screenBounds.getMaxX() - (30 * scalar),
+		// screenBounds.getMaxY() - (60 * scalar));
+		// stage.setResizable(false);
+		Scene scene = new Scene(root, screenBounds.getMaxX() - 20, screenBounds.getMaxY() - 30);
+
 		stage.setScene(scene);
 		stage.setMaximized(true);
 		stage.show();
