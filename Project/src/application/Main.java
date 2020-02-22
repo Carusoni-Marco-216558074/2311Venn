b4a938f71906cebc798c7ecaf5697493b47e8dd0
Carusoni-Marco-216558074 <marco.carusoni@gmail.com>
2020-02-22 17:13:04 -0500

    added better close handling

--- Project/src/application/Main.java
+++ Project/src/application/Main.java
@@ -1,10 +1,18 @@
 package application;
 
+import java.util.Optional;
+
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.ButtonType;
 
 public class Main extends Application {
 
@@ -17,6 +25,29 @@ public class Main extends Application {
 			primaryStage.setScene(scene);
 			primaryStage.setResizable(false);
 			primaryStage.show();
+			Platform.setImplicitExit(false);
+
+			primaryStage.setOnCloseRequest((EventHandler<WindowEvent>) new EventHandler<WindowEvent>() {
+				@Override
+				public void handle(WindowEvent t) {
+
+					Alert alert = new Alert(AlertType.CONFIRMATION);
+					alert.setTitle("Exit Venn Software");
+					alert.setHeaderText("Are you sure you want exit?");
+
+					Optional<ButtonType> result = alert.showAndWait();
+					if (result.get() == ButtonType.OK) {
+
+						System.exit(0);
+
+					} else {
+
+						t.consume();
+
+					}
+
+				}
+			});
 
 		} catch (Exception e) {
 			e.printStackTrace();
