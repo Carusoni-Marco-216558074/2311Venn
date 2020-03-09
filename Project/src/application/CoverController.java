040f626c7b1aa2499f9d908a7b114a685fde6102
spagnuolo-joseph-216279184 <46382058+spagnuolo-joseph-216279184@users.noreply.github.com>
2020-03-08 21:51:41 -0400

    opening

---
+++ Project/src/application/CoverController.java
@@ -0,0 +1,30 @@
+package application;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
+import javafx.animation.FadeTransition;
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
+public class CoverController {
+	
+    
+
+}
