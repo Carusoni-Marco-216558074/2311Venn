02bc48e39c13346bd2ce4cc008e34dcd9b141f21
spagnuolo-joseph-216279184 <46382058+spagnuolo-joseph-216279184@users.noreply.github.com>
2020-03-09 02:01:00 -0400

    Update CoverController.java

--- Project/src/application/CoverController.java
+++ Project/src/application/CoverController.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.net.URL;
+import java.util.Optional;
 import java.util.ResourceBundle;
 
 import javax.swing.JFileChooser;
@@ -13,17 +14,22 @@ import javafx.animation.FadeTransition;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+import javafx.stage.WindowEvent;
 import javafx.util.Duration;
 
 public class CoverController implements Initializable {
@@ -58,6 +64,26 @@ public class CoverController implements Initializable {
 						primaryStage.setTitle("Venn Diagram Software 2.0");
 						primaryStage.show();
 						parent.getScene().getWindow().hide();
+						primaryStage.setOnCloseRequest((EventHandler<WindowEvent>) new EventHandler<WindowEvent>() {
+							@Override
+							public void handle(WindowEvent t) {
+
+								Alert alert = new Alert(AlertType.CONFIRMATION);
+								alert.setTitle("Exit Venn Software");
+								alert.setHeaderText("Are you sure you want exit?");
+
+								Optional<ButtonType> result = alert.showAndWait();
+								if (result.get() == ButtonType.OK) {
+
+									System.exit(0);
+
+								} else {
+
+									t.consume();
+								}
+
+							}
+						});
 					}
 					
 				});
