351a70e68922174f1e0b46433ade8c41dfc5e74e
spagnuolo-joseph-216279184 <46382058+spagnuolo-joseph-216279184@users.noreply.github.com>
2020-03-22 22:52:17 -0400

    u

--- Project/src/application/OpeningController.java
+++ Project/src/application/OpeningController.java
@@ -39,15 +39,15 @@ public class OpeningController{
 		appStage.show();
 	}
 	
-//	@FXML
-//	private void openExistingProject(ActionEvent event) throws IOException {
-//		openExisting = true;
-//		Parent homePageParent = FXMLLoader.load(getClass().getResource("MainPage.fxml"));
-//		Scene homePageScene = new Scene(homePageParent);
-//		Stage appStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-//		appStage.setScene(homePageScene);
-//		appStage.centerOnScreen();
-//		appStage.show();
-//	}
+	@FXML
+	private void openExistingProject(ActionEvent event) throws IOException {
+		openExisting = true;
+		Parent homePageParent = FXMLLoader.load(getClass().getResource("MainPage.fxml"));
+		Scene homePageScene = new Scene(homePageParent);
+		Stage appStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+		appStage.setScene(homePageScene);
+		appStage.centerOnScreen();
+		appStage.show();
+	}
 
 }
